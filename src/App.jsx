ae62a3191503e5fc947b1725c35865650ff6bcc9
Alex <alexanderdiazdev@gmail.com>
2024-01-27 17:05:07 -0600

    ready to test

--- src/App.jsx
+++ src/App.jsx
@@ -7,71 +7,91 @@ function App() {
   const [showGame, setShowGame] = useState(false);
   return (
     <>
-      <Box height="100vh" width={500} padding="40px 10px">
-        <Box>
-          <Typography
-            variant="h4"
-            textAlign={"center"}
-            color={"white"}
-            mb={3}
-            display={showGame ? "none" : "block"}
-          >
-            ¿Cuanto sabes de perros? 🐶
-          </Typography>
-
-          <Box display={showGame ? "none" : "block"}>
+      {!showGame && (
+        <Box
+          width={500}
+          height={"100vh"}
+          display={"flex"}
+          alignItems={"center"}
+        >
+          <Box>
             <Typography
-              variant="subtitle1"
-              mb={1}
-              color="#bdbdbd"
-              sx={{ textDecoration: "underline" }}
+              variant="h4"
               textAlign={"center"}
+              color={"white"}
+              mb={3}
             >
-              Instrucciones:
+              ¿Cuanto sabes de perros? 🐶
             </Typography>
+
             <Box>
-              <Typography variant="body2" color="#bdbdbd" textAlign={"center"}>
-                Logra tu mayor record adivinando la raza de perro que veras en
-                la imagen.
-              </Typography>
-              <Typography variant="body2" color="#bdbdbd" mb={2} textAlign={"center"}>
-                <Typography
-                  component={"span"}
-                  mr="5px"
-                  color="#eeeeee"
-                  variant="body2"
-                >
-                  Tendras 10 puntos de bienvenida,
-                </Typography>
-                por cada falla, te restaremos 2 puntos en tu score y cada vez
-                que adivines
-                <Typography
-                  component={"span"}
-                  mx="5px"
-                  color="#eeeeee"
-                  variant="body2"
-                >
-                  Tendras 2 puntos extras en tu score!.
-                </Typography>
-              </Typography>
-            </Box>
-            <Typography variant="body1" color="white" mb={5} textAlign={"center"}>
-              Vamos! logra el mayor record posible 🎉
-            </Typography>
-            <Box display={"flex"} justifyContent={"center"}>
-              <Button
-                variant="contained"
-                color="success"
-                onClick={() => setShowGame(true)}
+              <Typography
+                variant="subtitle1"
+                mb={1}
+                color="#bdbdbd"
+                sx={{ textDecoration: "underline" }}
+                textAlign={"center"}
               >
-                Empezar!
-              </Button>
+                Instrucciones:
+              </Typography>
+              <Box>
+                <Typography
+                  variant="body2"
+                  color="#bdbdbd"
+                  textAlign={"center"}
+                >
+                  Logra tu mayor record adivinando la raza de perro que veras en
+                  la imagen.
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="#bdbdbd"
+                  mb={2}
+                  textAlign={"center"}
+                >
+                  <Typography
+                    component={"span"}
+                    mr="5px"
+                    color="#eeeeee"
+                    variant="body2"
+                  >
+                    Tendras 10 puntos de bienvenida,
+                  </Typography>
+                  por cada falla, te restaremos 2 puntos en tu score y cada vez
+                  que adivines
+                  <Typography
+                    component={"span"}
+                    mx="5px"
+                    color="#eeeeee"
+                    variant="body2"
+                  >
+                    Tendras 2 puntos extras en tu score!.
+                  </Typography>
+                </Typography>
+              </Box>
+              <Typography
+                variant="body1"
+                color="white"
+                mb={5}
+                textAlign={"center"}
+              >
+                Vamos! logra el mayor record posible 🎉
+              </Typography>
+              <Box display={"flex"} justifyContent={"center"}>
+                <Button
+                  variant="contained"
+                  color="success"
+                  onClick={() => setShowGame(true)}
+                >
+                  Empezar!
+                </Button>
+              </Box>
             </Box>
           </Box>
         </Box>
+      )}
 
-        {showGame && <Game />}
-      </Box>
+      {showGame && <Game />}
     </>
   );
 }
--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, CircularProgress } from "@mui/material";
 
 export default function Game() {
   const [score, setScore] = useState(10);
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState();
+  const [breedError, setBreedError] = useState(false);
 
   const loadBreeds = async () => {
     try {
@@ -22,51 +23,118 @@ export default function Game() {
   }, []);
 
   const updateScore = (answer) => {
-    console.log(answer)
-    if(answer){
-        setScore(score + 2)
-        loadBreeds();
-    }else{
-        setScore(score - 2)
+    if (answer) {
+      setBreedError(false);
+      setScore(score + 2);
+      loadBreeds();
+    } else {
+      setScore(score - 2);
+      setBreedError(true);
+      setTimeout(function () {
+        setBreedError(false);
+      }, 1500);
     }
-  }
+  };
+
   return (
-    <>
-      {isLoading && <Box color={"white"}>Loading......</Box>}
-
-      {!isLoading && data && (
-        <Box>
-          <Box mb={4}>
-            <Typography variant="h5" textAlign={"center"} color={"white"}>
-              Tienes: {score} puntos
+    <Box height={"100vh"} width={500} pt={5}>
+      {score > 0 && (
+        <Box mb={3} display={"flex"}>
+          <Typography
+            bgcolor={"#1B5E21"}
+            py={1}
+            px={3}
+            color={"white"}
+            borderRadius={"4px"}
+          >
+            Tu puntaje actual:
+            <Typography variant="span" px={"5px"} fontSize={"20px"}>
+              {score}
             </Typography>
-          </Box>
-
-          <Box display={"flex"} justifyContent={"center"} mb={2}>
-            <Box width={300}>
-              <img src={data.image} width="100%" />
-            </Box>
-          </Box>
-          <Box display={"flex"} justifyContent={"center"}>
-            <Box width={300}>
-              {data.options.map(({ breed, correct }, index) => {
-                return (
-                  <Box key={index} mb={1}>
-                    <Button
-                      variant="contained"
-                      color="success"
-                      onClick={() => updateScore(correct)}
-                      fullWidth
-                    >
-                      {breed}
-                    </Button>
-                  </Box>
-                );
-              })}
-            </Box>
-          </Box>
+          </Typography>
         </Box>
       )}
-    </>
+      <Box>
+        {isLoading && (
+          <Box display={"flex"} justifyContent={"center"} pt={10}>
+            <CircularProgress />
+          </Box>
+        )}
+        {score === 0 && (
+          <Box pt={10}>
+            <Box mb={5}>
+              <Typography color={"white"} textAlign={"center"} variant="h4">
+                Tu score llego a cero
+              </Typography>
+              <Typography
+                color={"white"}
+                textAlign={"center"}
+                variant="subtitle1"
+              >
+                ¿Quieres intentarlo de nuevo?
+              </Typography>
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  setScore(10), loadBreeds();
+                }}
+                fullWidth
+              >
+                Jugar de nuevo!
+              </Button>
+            </Box>
+          </Box>
+        )}
+        {data && !isLoading && score > 0 && (
+          <Box>
+            <Typography
+              variant="h6"
+              color={"white"}
+              textAlign={"center"}
+              mb={3}
+            >
+              ¿Que raza de perro es?
+            </Typography>
+            <Box>
+              <Box display={"flex"} mb={2} justifyContent={"center"}>
+                <Box width={"400px"}>
+                  <Box mb={1}>
+                    <img src={data.image} width={"100%"} />
+                  </Box>
+                  {breedError && (
+                    <Box>
+                      <Typography textAlign={"center"} color={"#f73378"}>
+                        No es la raza correcta 🙁
+                      </Typography>
+                    </Box>
+                  )}
+                </Box>
+              </Box>
+              <Box display={"flex"} justifyContent={"center"}>
+                <Box width={"400px"}>
+                  {data.options.map(({ breed, correct }, index) => {
+                    return (
+                      <Box mb={1} key={index}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          onClick={() => updateScore(correct)}
+                          fullWidth
+                        >
+                          {breed}
+                        </Button>
+                      </Box>
+                    );
+                  })}
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        )}
+      </Box>
+    </Box>
   );
 }
